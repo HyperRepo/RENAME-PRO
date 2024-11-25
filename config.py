3cd37cf21f49a8fde36b9713dc4ba150acc3622b
Hyper <145529358+HyperRepo@users.noreply.github.com>
2024-11-25 20:27:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-OWNER = int(os.environ.get("OWNER", ""))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7594768624:AAHp7_xBi2iXy5iZh7WqEjEsV6Vq5jdM2a4")
+API_ID = int(os.environ.get("API_ID", "20562331"))
+API_HASH = os.environ.get("API_HASH", "9e3e4148e73756a85b95fc69980b678d")
+OWNER = int(os.environ.get("OWNER", "2107586423"))
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "Hyper_Rename_Bot")
 
-FORCE_SUBS = os.environ.get("FORCE_SUBS", "HxBots")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002031041556"))
+FORCE_SUBS = os.environ.get("FORCE_SUBS", "-1002315795055")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002390640098"))
 
-DB_URL = os.environ.get("DB_URL", "")
-DB_NAME = os.environ.get("DB_NAME", "renamebot-premium")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://deepchutia:deepchutia@123@cluster0.eqcc3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DB_NAME", "Cluster0")
 
-STRING = os.environ.get("STRING", "")
+STRING = os.environ.get("STRING", "BQE5wZsAWVhZsiodpb25LlZWW3uOCv2p3g32ttOUrIbta2BPLBgDCXxBVSl-b19uLwdR00JyYtMc0VPhujka-hmSOPFF7lzmXoOq4MQve_qdx7qCvu8dTZX-8RHg9LuA97lgTWPU5_BFO8O7dkAbAxzHPes6hZRAvScT7AV7PPDF5_BL0lv8QegU9ObWT2U6vt0gg_KEBK_-EQ8VepEY9oDECsawMe7Ro8TY6GyQ_FVEXo3hlP6e5z7s1Sn72dsRMqk--EHbnf17BYWvkSwMlFFbES-Aoodp8BWwuERMNMAtq5mswf0HgFptH6ej-iFz3AoKgOSt2FuU_QzEhJkdFG69jHVblgAAAAB9nzd3AA")
 BOT_PIC = os.environ.get("BOT_PIC", "https://graph.org/file/cca849a2f63053fa3f622.jpg")
 
 
